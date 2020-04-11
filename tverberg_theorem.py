dcd2998528aee173530373e00bb111254db0c79f
KukaiN <50426885+kukai10@users.noreply.github.com>
2020-04-10 23:04:54 -0400

    update

--- tverberg_theorem.py
+++ tverberg_theorem.py
@@ -5,32 +5,13 @@ def set_of_points(min_num, max_num, dimensions, num_of_points):
 
 def check_tverberg(list_of_points, num_of_points):
     for index, list_b in enumerate(list_of_points):
-        if index %10000 == 0:print("working on", index)
-        if check_size1(list_b, num_of_points):
-            yield list_b
-    print(index)
-
-
-def check_tverberg2(list_of_points, num_of_points):
-    for index, list_b in enumerate(list_of_points):
         if index %100000 == 0:print("working on", index)
-        if check_size2(list_b, num_of_points):
+        if check_size(list_b, num_of_points):
             yield list_b
     print(index)
 
-def check_size1(list_of_points, size):
-    check = True
-    for i in range(size-1):
-        if check:
-            for j in range(i+1, size):
-                point_a = list_of_points[i] 
-                point_b = list_of_points[j]
-                for a, b in zip(point_a, point_b):
-                    if a == b: check = False
-    return check
-
-
-def check_size2(list_of_points, size):
+def check_size(list_of_points, size):
+    # returns the bool of the size and the size after hashing
     return len(set(list_of_points)) == size 
 
 def main():
@@ -50,17 +31,7 @@ nump = 5
 for i, a in  enumerate(set_of_points(0, 10, 2, nump)):
     pass
 print(i)
-print(time.time() - t2)
-"""
-for a in check_tverberg(ls, nump):
-    pass
-t2 = time.time()
-print(t2-t1)
-ls = set_of_points(0, 10, 2, nump)
-for a in check_tverberg2(ls, nump):
-    pass
-print(time.time()-t2)
+print(time.time() - t1)
 
 #if __name__ == "__main__":
 #    main()
-"""
---
+++ visualize_convex_sets.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy
+import random
+import itertools
+
+def visualize(input_data, dimensions, max=None, min=None, pref_color=None):
+
+
+
+def main():
+    for points in [itertools.product]
+
+if __name__ == "__main__":
+    main()
