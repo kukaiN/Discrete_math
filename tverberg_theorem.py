e6b2422f77d7e39ed853bbbde7eb1413a2fa72d6
KukaiN <50426885+kukai10@users.noreply.github.com>
2020-04-14 20:17:14 -0400

    update

--- tverberg_theorem.py
+++ tverberg_theorem.py
@@ -1,7 +1,60 @@
 from itertools import combinations, product
+import operator
+import math
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+import visualize_convex_sets as visual
 
 def set_of_points(min_num, max_num, dimensions, num_of_points):
     return combinations(product(range(min_num, max_num+1), repeat=dimensions), num_of_points)
+
+def new_set_of_points(min_num, max_num, num_of_points,  dimensions=2):
+    for _ in range(num_of_points):
+        pass    
+
+
+def ccw(base_point, p1, p2):
+    # use the cross product to get z value and check the sign
+    return (p1[0]-base_point[0])*(p2[1]-base_point[1]) - (p1[1]-base_point[1])*(p2[0]-base_point[0])
+
+
+
+
+
+def grahams_method(set_of_points, list_size, min_x = None, max_x = None, sorted=True):
+    if not sorted:
+        set_of_points.sort(key= lambda a: a[1])
+    smallest_point = min(set_of_points, key=lambda a: a[1])
+    smallest_y = smallest_point[1]
+    candidates = [tup for tup in set_of_points if tup[1] == smallest_y]
+    candidates.sort(key = lambda a: a[0])
+    candidates.reverse()
+    starting_point = candidates[0]    
+    x_coor, y_coor  = starting_point
+    base_vec = [1, 0]
+    absolute_points = [point for point in set_of_points if point != starting_point]
+    relative_points = [[coor[0]-x_coor, coor[1]-y_coor] for coor in absolute_points]
+    angles = [np.arccos(np.dot(base_vec, vec/np.linalg.norm(vec))) for vec in relative_points]
+    angles_and_coor = [[ang, coor] for ang, coor in zip(angles, absolute_points)]
+    angles_and_coor.sort(key = lambda a: a[0])
+    print([a[0] for a in angles_and_coor])
+    stack = [starting_point]
+    for coor in angles_and_coor:
+        while len(stack) > 1 and ccw(stack[-2], starting_point, stack[-1]) < 0:
+            del stack[-1]
+        stack.append(coor[1])
+        print(stack)
+
+    visual.visualize(set_of_points, stack, 2, maxVal=max_x, minVal=min_x)
+
+num_of_points = 12
+min_x, max_x = 0 , 30 
+stuff = [(random.randint(min_x, max_x), random.randint(min_x, max_x)) for _ in range(num_of_points)]
+stuff = list(set(stuff))
+print(stuff)
+grahams_method(stuff, num_of_points,min_x, max_x, sorted=False)
+
 
 def check_tverberg(list_of_points, num_of_points):
     for index, list_b in enumerate(list_of_points):
@@ -19,11 +72,11 @@ def main():
     min_num = 0
     dimensions = 2
     num_of_points = 5
-    for a in check_tverberg(set_of_points(min_num, max_num, dimensions, num_of_points), num_of_points):
-        pass
+    #for a in check_tverberg(set_of_points(min_num, max_num, dimensions, num_of_points), num_of_points):
+    #    pass
     print("finished")
 
-
+"""
 import time
 
 t1 = time.time()
@@ -32,6 +85,6 @@ for i, a in  enumerate(set_of_points(0, 10, 2, nump)):
     pass
 print(i)
 print(time.time() - t1)
-
-#if __name__ == "__main__":
-#    main()
+"""
+if __name__ == "__main__":
+    main()
--- visualize_convex_sets.py
+++ visualize_convex_sets.py
@@ -1,14 +1,34 @@
 import matplotlib.pyplot as plt
-import numpy
+import numpy as np
 import random
 import itertools
 
-def visualize(input_data, dimensions, max=None, min=None, pref_color=None):
+def visualize(points, convex_border, dimensions, maxVal=None, minVal=None, linestyles=["r.","b-", "rx"], ticks=[5, 1]):
+    if maxVal == None: maxVal = max(max(points, key = lambda a: a[0]), max(points, key = lambda a: a[1]))
+    if minVal == None: minVal = min(min(points, key = lambda a: a[0]), min(points, key = lambda a: a[1]))
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    major_ticks = np.arange(minVal, maxVal+1, ticks[0])
+    minor_ticks = np.arange(minVal, maxVal+1, ticks[1])
+    ax.set_xticks(major_ticks)
+    ax.set_xticks(minor_ticks, minor=True)
+    ax.set_yticks(major_ticks)
+    ax.set_yticks(minor_ticks, minor=True)
+    ax.grid(which="both")
 
+    x_points, y_points = [a[0] for a in points], [a[1] for a in points]
+    conv_x_points, conv_y_points = [a[0] for a in convex_border], [a[1] for a in convex_border]
+    plt.plot(x_points, y_points, linestyles[0])
+    plt.plot(conv_x_points, conv_y_points, linestyles[1])
+    plt.plot(conv_x_points, conv_y_points, linestyles[2])
+    plt.show()
 
 
 def main():
-    for points in [itertools.product]
+    #for points in [itertools.product]:
+    #    pass
+    pass
 
 if __name__ == "__main__":
     main()
