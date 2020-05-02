319d5bbb2e8b9858c065551ada7e3272ccac8ddb
KukaiN <50426885+kukai10@users.noreply.github.com>
2020-05-02 17:16:20 -0400

    update

---
+++ test.py
@@ -0,0 +1,7 @@
+import math
+
+
+x = -5
+y = 20
+z = math.gcd(x, y)
+print(z)
--- tverberg_theorem.py
+++ tverberg_theorem.py
@@ -26,12 +26,17 @@ def make_angle_list(point_list, lowest_point):
     # there's a division by zero for normal atan(y / x) if the point in consideration is directly above
     return [(math.atan2(point[1]-low_y, point[0]-low_x), point) for point in point_list]
 
-def grahams_method(set_of_points, list_size, min_x = None, max_x = None, list_sorted=True):
+def get_smallest_point(set_of_points):
     # getting the smallest element, O(n)
     smallest_point = set_of_points[0]
     for point in set_of_points:
         if smallest_point[1]+1>point[1] and smallest_point[0]+1 > point[0]:
             smallest_point = point
+    return smallest_point
+
+def grahams_method(set_of_points, list_size, min_x = None, max_x = None, list_sorted=True):
+    
+    smallest_point = get_smallest_point(set_of_points)
 
     # making a dictionary with angles from the smallest point
     angles_dict = dict()
@@ -69,6 +74,7 @@ def load_pickle(filepath, default):
             x = pickle.load(f)
     except Exception:
         x = default
+        print("exception")
         with open(path, "wb") as f:
             pickle.dump(x, f)
     return x
@@ -82,57 +88,142 @@ def get_cd():
     if os.getcwd() != scriptpath: filename = scriptpath + "\\"
     return filename
 
+def reducible_slope(del_x, del_y):
+    # simple check to see if the partition is even worthy of checking
+    reduced_slope = math.gcd(del_x, del_y) 
+    if reduced_slope == 1:
+        return 0
+    else: # if there's a reducible slope, then theres an integer point in between, given both input points have integer coordinates
+        return reduced_slope
+    
+
+def find_examples(min_x, max_x, number_of_points, pickle_name):
+    t1 = time.time()
+    data_points = []
+
+    for index, candidate_points in enumerate(set_of_points(min_x, max_x, 2,  number_of_points)):  
+        if index%100000 == 0: print(index)
+        break_bool = False
+        for partition in combinations(candidate_points, 2): # partition contains two points that we want to use
+            # find the slope of the integer points
+            p1 = partition[0]
+            p2 = partition[1]
+            delta_x = p2[0] - p1[0]
+            delta_y = p2[1] - p1[1]
+            points_in_between = reducible_slope(delta_x, delta_y)
+            
+            # integer points on the line
+            points = [[p1[0]+i*delta_x, p1[1]+i*delta_y] for i in range(points_in_between+1)] if points_in_between else partition
+            # the other set of 3 points that makes a convex hull
+            other_partition = np.array([point for point in candidate_points if point not in partition])
+
+            for point in points:
+                if in_hull(other_partition, np.array(point)):
+                        break_bool = True
+                if break_bool:
+                    break
+            # since we need a set that dosnt have an integer point we need to exit out of the loop again
+            if break_bool:
+                break
+        # go to next candidate points
+
+        if not break_bool: # we couldnt find integer points so we found an example     
+            data_points.append(list(candidate_points))
+
+    if data_points != []:
+        df = pd.DataFrame(data_points, columns=["p1", "p2", "p3", "p4", "p5"])
+        save_pickle(pickle_name, df)
+    time_took = t1-time.time() 
+    print(time_took)
+    print("index", index)
+
+
+def filter_same_shape(data, min_x, max_x):
+    base_shape = data[0]
+    set_of_shapes = []
+    for convex_set in data:
+        outside_stack = grahams_method(convex_set, 1, min_x, max_x)[0]
+        angles_list = []
+        length_list = []
+        stack_size = len(outside_stack)
+        for i in range(stack_size):
+            #print(outside_stack)
+            x1, y1 = outside_stack[i%stack_size]
+            x2, y2 = outside_stack[(i+1)%stack_size]
+            length_list.append(math.hypot(x2-x1, y2-y1))
+        if set_of_shapes == []:
+            set_of_shapes.append((convex_set , length_list))
+        else:
+            # check the distances between points if they're the same, then they're the same 
+            temp_bool = True
+            for tup_item in set_of_shapes:
+                if set(tup_item[1]) != set(length_list):
+                    temp_bool = False
+            if temp_bool:
+                set_of_shapes.append((convex_set , length_list))
+
+    print(len(set_of_shapes))
+    return set_of_shapes
+
+def filter_same_shape2(data, min_x, max_x):
+    unique_sets = []
+    for potential_set in data:
+        smallest_point = get_smallest_point(potential_set)
+        x_sm, y_sm = smallest_point
+        scaled_set = [(x-x_sm, y-y_sm) for (x, y) in data]
+        break_bool = True
+        for known_set in unique_sets:
+            if break_bool:
+                if set(known_set) == set(scaled_data):
+                    break_bool = False
+                    break
+        if break_bool:
+            unique_sets.append(scaled_data)
+    print(len(unique_sets))
+    return unique_sets
+
+def play_with_pickle(current_dir, min_x, max_x):
+    # open pkl file
+    data = load_pickle(current_dir+"tverberg_keep7.pkl", [])
+    new_data = []
+    for index, row in data.iterrows():
+        current_row = []
+        for pt in row:
+            current_row.append(pt)
+        new_data.append(current_row)
+        
+    new_data = filter_same_shape(new_data, min_x, max_x)
+    #print(new_data)
+    counter = 0
+    for data in new_data:
+        
+        smallest_point = get_smallest_point(data[0])
+        visual.visualize(data[0], data[0], 2, maxVal=max_x, minVal=min_x, angles=True, min_point=smallest_point, other_point=True)
+
 def main():
     current_dir = get_cd()
     num_of_points = 10
     min_x, max_x = 0 , 7
     dimensions = 2
-    repetition = 10
+    repetition = 1
     pickle_name = current_dir+"tverberg"
-    pickle_type = ".pkl"
+    pickle_name+=".pkl"
     """
     for i in range(repetition):
         stuff = list(set((random.randint(min_x, max_x), random.randint(min_x, max_x)) for _ in range(num_of_points)))
         exterior_set, smallest_point = grahams_method(stuff, num_of_points,min_x, max_x, list_sorted=False)
- 
-        #convex_set = np.random.rand(num_of_points, 2)
-        #point_in_set = np.random.rand(dimensions)
+
         convex_set = np.array(stuff)
         point_in_set = np.random.randint(max_x+1, size=2)
 
         print(in_hull(convex_set, point_in_set))
-        #visual.visualize(stuff, exterior_set, 2, maxVal=max_x, minVal=min_x, angles=False, min_point=smallest_point, other_point=True, point_considered=point_in_set)
+        #visual.visualize(stuff, exterior_set, 2, maxVal=max_x, minVal=min_x, angles=True, min_point=smallest_point, other_point=True, point_considered=point_in_set)
     """
-    max_x = 8
-    number_of_points = 4
-    t1 = time.time()
-    points_Z2 = {(i, j) for i in range(max_x+1) for j in range(max_x+1)} 
-    data_points = []
-    #print(list(set_of_points(min_x, max_x, 2, number_of_points)))
-    for i in (set_of_points(min_x, max_x, 2,  number_of_points)):
-        if not ((0, 0) in i):
-            candidate_set = [(0, 0)] + list(i)  
-            break_bool = True
-            for point in points_Z2 - set(candidate_set):
-                if in_hull(np.array(candidate_set), np.array(point)):
-                    break_bool = False
-                    break
-            if break_bool:
-                data_points.append(candidate_set)
-    if data_points != []:
-        df = pd.DataFrame(data_points, columns=["p1", "p2", "p3", "p4", "p5"])
-        save_pickle(pickle_name+".pkl", df)
-    time_took = t1-time.time() 
-    print(time_took)
-    """
-    import time
+    max_x = 6
+    number_of_points = 5
+    #find_examples(min_x, max_x, number_of_points, pickle_name)
+    play_with_pickle(current_dir, min_x, max_x)
 
-    t1 = time.time()
-    nump = 5
-    for i, a in  enumerate(set_of_points(0, 10, 2, nump)):
-        pass
-    print(i)
-    print(time.time() - t1)
-    """
+    
 if __name__ == "__main__":
     main()
