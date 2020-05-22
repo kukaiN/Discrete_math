797a0e75ea0b422b725fda0a778df337fb9c466c
KukaiN <50426885+kukai10@users.noreply.github.com>
2020-05-21 21:32:52 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
-import math
+import numpy as np
+import cupy as cp
 
+print("hello")
+x_gpu = cp.array([1, 2, 3])
 
-x = -5
-y = 20
-z = math.gcd(x, y)
-print(z)
+x_cpu = np.array([1, 2, 3])
+l2_cpu = np.linalg.norm(x_cpu)
+
+l2_gpu = cp.linalg.norm(x_gpu)
+print("jesus")
